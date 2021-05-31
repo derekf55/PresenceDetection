fbddea38ebd121cb4b232a54eb6a5d91fb15809f
derekf55 <derekfran55@gmail.com>
2021-05-31 15:31:09 -0500

    Tried fixing bug

--- personDetection.py
+++ personDetection.py
@@ -76,10 +76,10 @@ def createPeopleToNoticeDatabase():
     global KNOWN_PEOPLE
     priority = df.runSql("SELECT PriorityLevel FROM personDetectionPriority WHERE ID = 1")
     if priority[0][0] == "Disabled":
-        KNOWN_PEOPLE = []
+        PEOPLE_TO_NOTICE = []
         return
     elif priority[0][0] == "Home Alone":
-        KNOWN_PEOPLE = []
+        PEOPLE_TO_NOTICE = []
         for person in KNOWN_PEOPLE:
             if person['Name'] == 'Derek':
                 continue
